09199374e81c475951bc87f78ce086a8c7fac5bc
Dianet Utria <dia.utria@gmail.com>
2026-05-13 12:01:29 -0300

    Para que funcione el boton atras en android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  standalone: true,
+  imports: [
+    IonApp,
+    IonRouterOutlet
+  ]
 })
 export class AppComponent {
-  constructor() {}
+  @ViewChild(IonRouterOutlet, { static: true })
+  private routerOutlet!: IonRouterOutlet;
+
+  private platform = inject(Platform);
+
+  constructor() {
+    this.initializeApp();
+  }
+
+  private initializeApp() {
+    this.platform.ready().then(() => {
+      this.configurarBotonAtrasAndroid();
+    });
+  }
+
+  private configurarBotonAtrasAndroid() {
+    this.platform.backButton.subscribeWithPriority(10, async () => {
+      if (this.routerOutlet?.canGoBack()) {
+        await this.routerOutlet.pop();
+        return;
+      }
+
+      await App.exitApp();
+    });
+  }
 }
